354bc889bd38612642e569bfad276ebb95299424
Sergiu <ctinsergiu@gmail.com>
2019-09-10 22:14:50 +0300

    Implemented CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -26,6 +27,12 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // GLOBAL MIDDLEWARES
+// implement cors
+app.use(cors());
+// Access-Control-Allow-Origin
+
+app.options('*', cors());
+// app.options('/api/v1/tours/:id', cors());
 
 // getting the data from static file
 // app.use(express.static(`${__dirname}/public`));
